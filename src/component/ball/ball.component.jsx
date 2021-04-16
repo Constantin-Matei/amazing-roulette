74f2925f06445e856444130b0cecf5fcc0be60af
Constantin Matei <constantin@nepretuit.ro>
2021-04-16 16:33:19 +0100

    I generated the ball to stop inside the roulette wheel at a random position.

--- src/component/ball/ball.component.jsx
+++ src/component/ball/ball.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { StyledBall } from './ball.style'
 
-const Ball = () => {
+const Ball = ({ rotation }) => {
   return (
-    <StyledBall />
+    <StyledBall rotation={rotation}/>
   )
 }
 
